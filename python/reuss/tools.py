d9e3ad88c7e4d92547f030f1917252248a958d98
Erik Frojdh <erik.frojdh@psi.ch>
2022-08-16 10:16:17 +0200

    WIP

--- python/reuss/tools.py
+++ python/reuss/tools.py
@@ -1,10 +1,26 @@
 import shutil
 from pathlib import Path
-
+import sys, tty, termios
 from .formatting import color
+
+
 def default_config():
     dst = Path().home()/'.reussrc'
     src = Path(__file__).parent/'reussrc.in'
     print(f'Copying: {src} to {dst}')
     shutil.copy(src, dst)
     print(color.yellow("Adapt ~/.reussrc to your detector before proceeding"))
+
+
+def getch():
+    fd = sys.stdin.fileno()
+    old = termios.tcgetattr(fd)
+    new = termios.tcgetattr(fd)
+    new[3] = new[3] & ~termios.ECHO
+    new[3] = new[3] & ~termios.ICANON
+    try:
+        termios.tcsetattr(fd, termios.TCSADRAIN, new)
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old)
+    return ch
