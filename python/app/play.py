1d8ef77653e615618faf1f8edf6db0942c76b53b
Erik Frojdh <erik.frojdh@psi.ch>
2022-08-19 07:56:36 +0200

    trigger option in script

--- python/app/play.py
+++ python/app/play.py
@@ -9,25 +9,27 @@ import sys
 from pathlib import Path
 
 from reuss import Receiver
-from slsdet import Detector, runStatus
+from slsdet import Detector, runStatus, timingMode
 
 
 
 
 d = Detector()
-
 r = Receiver("tcp://127.0.0.1:5556") #where to talk to the receiver
 
 r.udp_source = f"{d.udp_dstip}:{d.udp_dstport}"
-r.frames = d.frames
 
+if d.timing==timingMode.TRIGGER_EXPOSURE:
+    r.frames = d.triggers
+else:
+    r.frames = d.frames
 
 def acquire(det, rcv):
     rcv.start() 
     time.sleep(0.1)
     det.start()
     time.sleep(0.1)
-    while det.status == runStatus.RUNNING:
+    while det.status != runStatus.IDLE:
         print(f"{rcv.progress*100:3.0f}%", end = '\r')
         time.sleep(0.1)
     print('')
@@ -35,7 +37,9 @@ def acquire(det, rcv):
     rcv.stop()
 
 r.fpath = '/home/l_msdetect/erik/tmp/g2'
-r.fwrite = True
+r.fwrite = False
+
+
 acquire(d,r)
 
 
