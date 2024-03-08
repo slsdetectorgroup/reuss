17377689312aabe3ca28163b587d812039943ef0
Erik Fröjdh <erik.frojdh@gmail.com>
2024-03-08 11:09:50 +0100

    added stream replay

---
+++ python/app/replay.py
@@ -0,0 +1,32 @@
+import argparse
+import numpy as np
+from pathlib import Path
+import zmq
+import time
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Replay a saved stream")
+    parser.add_argument("path", type=Path, help="Path to the saved stream")
+    parser.add_argument("--fps", type=int, default=10, help="Frames per second")
+    parser.add_argument("--port", type=int, default=4545, help="Port to bind to")
+
+    args = parser.parse_args()
+
+    data = np.load(args.path) #TODO! avoid loading full file? 
+    
+
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind(f"tcp://*:{args.port}")
+    
+    frame_number = np.int64(1)
+    frame_index = 0
+    while True:
+        socket.send_multipart([frame_number.tobytes(), data[frame_index].tobytes()])
+        frame_index += 1
+        if frame_index == data.shape[0]:
+            frame_index = 0
+        frame_number += 1
+        time.sleep(1/args.fps)
+        print(frame_number, end="\r")
