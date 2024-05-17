8315e3e795f588d1cce4cf49bc652d680a25c25f
Erik Frojdh <erik.frojdh@gmail.com>
2024-05-17 16:34:01 +0200

    WIP

--- python/reuss/config.py
+++ python/reuss/config.py
@@ -44,7 +44,7 @@ bitmask = np.array([0x3FFF], dtype=np.uint16)
 
 #TODO! Synch with C++
 #roi = [(slice(0, 512, 1), slice(256, 768, 1))]
-roi = [(slice(0, 512, 1), slice(0, 1024, 1))]
+roi = [(slice(0, 514, 1), slice(0, 1030, 1))]
 
 class index(Enum):
     ROW = 0
